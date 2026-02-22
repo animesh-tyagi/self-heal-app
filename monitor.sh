5f86afdebc19227a7f6285e783f59c3469c961f6
animesh-tyagi <animesh.tyagi.720@gmail.com>
2026-02-22 18:54:46 +0000

    removed memory usage logging from monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -12,9 +12,3 @@ if [ "$STATUS" != "200" ]; then
     echo "$(date): Service unhealthy. Restarting container...">> $LOGFILE
     docker restart heal-container >> $LOGFILE
 fi
-
-MEM=$(free -m | awk '/Mem:/ {print $3}')
-
-if [ "$MEM" -gt 400 ]; then
-    echo "$(date): High memory usage: $MEM MB" >> $LOGFILE
-fi
